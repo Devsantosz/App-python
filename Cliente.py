393a4ff051cf5b79c222c04418d2fe28e7931096
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-01-19 23:14:22 -0300

    Modulo 03 - encapsulamento de dados e de como a linguagem Python trabalha de forma diferenciada das outras linguagens

--- Cliente.py
+++ Cliente.py
@@ -1,5 +1,14 @@
 class Cliente:
     def __init__(self, n,telefone):
 
-         self.nome = n
-         self.telefone = telefone
+         self._nome = n
+         self._telefone = telefone
+
+
+#Metodo get
+    def get_nome(self):
+        return self._nome
+
+#Metodo set
+    def set_nome(self, nome):
+        self._nome = nome
